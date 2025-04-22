01303dcdd25bb5fe927204a3e189f9783a5a5b8a
Gab0CS <gabocanseco@gmail.com>
2025-04-22 00:13:46 -0600

    filter implemented for OAuth2.0 in gateway beans

--- src/main/java/com/gabo/gateway/beans/GatewayBeans.java
+++ src/main/java/com/gabo/gateway/beans/GatewayBeans.java
@@ -8,9 +8,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.gabo.gateway.filters.AuthFilter;
+
+import lombok.AllArgsConstructor;
+
 @Configuration
+@AllArgsConstructor
 public class GatewayBeans{
     
+    private final AuthFilter authFilter;
+
     @Bean
     @Profile(value = "eureka-off")
     public RouteLocator routeLocatorEurekaOff(RouteLocatorBuilder builder){
@@ -68,5 +75,39 @@ public class GatewayBeans{
             .uri("lb://companies-crud-fallback")
         )
         .build();
+    }
+
+    @Bean
+    @Profile(value = "oauth2")
+    public RouteLocator routeLocatorOauth2(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                .route(route -> route
+                        .path("/companies-crud/company/**")
+                        .filters(filter -> {
+                            filter.circuitBreaker(config -> config
+                                    .setName("gateway-cb")
+                                    .setStatusCodes(Set.of("500", "400"))
+                                    .setFallbackUri("forward:/companies-crud-fallback/company/*"));
+                            filter.filter(this.authFilter);
+                            return filter;
+                        })
+                        .uri("lb://companies-crud")
+                )
+                .route(route -> route
+                        .path("/report-ms/report/**")
+                        .filters(filter -> filter.filter(this.authFilter))
+                        .uri("lb://report-ms")
+                )
+                .route(route -> route
+                        .path("/companies-crud-fallback/company/**")
+                        .filters(filter -> filter.filter(this.authFilter))
+                        .uri("lb://companies-crud-fallback")
+                )
+                .route(route -> route
+                        .path("/auth-server/auth/**")
+                        .uri("lb://auth-server")
+                )
+                .build();
     }
 }
